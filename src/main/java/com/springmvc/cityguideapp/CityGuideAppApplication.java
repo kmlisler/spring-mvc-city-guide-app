e5cef58476c97da4559453abc4add14f1b395637
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-12-20 22:29:58 +0300

    Initial commit

---
+++ src/main/java/com/springmvc/cityguideapp/CityGuideAppApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.cityguideapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityGuideAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityGuideAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvc/cityguideapp/CityGuideAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.cityguideapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityGuideAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
