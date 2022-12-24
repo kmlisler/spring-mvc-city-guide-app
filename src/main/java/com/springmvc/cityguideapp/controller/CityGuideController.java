6debc765f92ebf33db0ed58c6a5fd30e85bf0aad
SametCer <smtcrzc@gmail.com>
2022-12-24 19:09:05 +0300

    commit

--- src/main/java/com/springmvc/cityguideapp/controller/CityGuideController.java
+++ src/main/java/com/springmvc/cityguideapp/controller/CityGuideController.java
@@ -6,10 +6,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CityGuideController {
 
-    @GetMapping("/main")
+    @GetMapping("/")
+    public String home(String string){
+
+        return "home";
+    }
+    @GetMapping("/allAttractions")
+    public String allAttractions(String string){
+
+        return "all-attractions";
+    }
+    @GetMapping("/nearbyAttractions")
     public String nearbyAttractions(String string){
 
         return "nearby-attractions";
+    }
+    @GetMapping("/newsEventsTips")
+    public String newsEventsTips(String string){
+
+        return "news-events-tips";
+    }
+    @GetMapping("/purchaseCard")
+    public String purchaseCard(String string){
+
+        return "purchase-card";
+    }
+    @GetMapping("/weather")
+    public String weather(String string){
+
+        return "weather";
+    }
+    @GetMapping("/whereToEatAndStay")
+    public String whereToEatAndStay(String string){
+
+        return "where-to-eat-and-stay";
     }
 
 }
